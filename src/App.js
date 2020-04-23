94aa12c17aed031ef049146b3aaf28d46d5d9692
Nathaniel Kessler <hey@nathanielkessler.com>
2020-04-23 16:54:03 -0400

    adds tailwind and storybook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { ButtonPrimary } from './ui/components/button-primary';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-sm rounded shadow-lg">
+      <ButtonPrimary />
     </div>
   );
 }
--- src/ui/components/button-primary.js
+++ src/ui/components/button-primary.js
@@ -1,3 +1,3 @@
 import React from 'react';
 
-export const ButtonPrimary = () => <div>button primary</div>
+export const ButtonPrimary = () => <div className="max-w-sm rounded shadow-lg px-6 py-4">button primary</div>
