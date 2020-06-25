e9b3368d84593a5987e3956819c377cbfaa8ff92
Sayuri Mizuguchi <adoravel_sansei@hotmail.com>
2020-06-25 15:34:47 -0400

    allowing components to accept testId property

--- src/design-system/components/button-secondary.js
+++ src/design-system/components/button-secondary.js
@@ -4,5 +4,6 @@ export const ButtonSecondary = ({
   className = '',
   onClick = () => {},
   children,
+  testId,
   disabled = false,
-}) => <button onClick={onClick} disabled={disabled} className={`max-w-sm flex items-center rounded-lg shadow-lg px-5 py-3 bg-black text-white copy-bold ${className}`}>{children}</button>
+}) => <button data-cy-id={testId} onClick={onClick} disabled={disabled} className={`max-w-sm flex items-center rounded-lg shadow-lg px-5 py-3 bg-black text-white copy-bold ${className}`}>{children}</button>
